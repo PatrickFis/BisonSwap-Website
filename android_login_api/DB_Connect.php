010916c1b54a44e8f7bc1d7cf79e8005f2c79a87
Patrick Fischer <fischerpl18@gmail.com>
2017-01-25 14:39:37 -0600

    Initial commit of Android login API. Will need to modify code a decent amount.

--- android_login_api/DB_Connect.php
+++ android_login_api/DB_Connect.php
@@ -1,4 +1,17 @@
 <?php
-	class DB_Connect {
+class DB_Connect {
+    private $conn;
+
+    // Connecting to database
+    public function connect() {
+        require_once '../dbconnect.php';
+
+        // Connecting to mysql database
+        $this->conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+
+        // return database handler
+        return $this->conn;
+    }
 }
+
 ?>
