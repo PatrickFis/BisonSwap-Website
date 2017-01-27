f4b695e31282ce9a26f7e661b2b4d7feba4855d0
Patrick Fischer <fischerpl18@gmail.com>
2017-01-27 14:54:05 -0600

    Modifying db connect

--- android_login_api/DB_Connect.php
+++ android_login_api/DB_Connect.php
@@ -4,9 +4,13 @@ class DB_Connect {
 
     // Connecting to database
     public function connect() {
-        require_once '../dbconnect.php';
+        // require_once '../dbconnect.php';
 
         // Connecting to mysql database
+				$DBHOST = 'localhost';
+				$DBUSER = 'root';
+				$DBPASS = '';
+				$DBNAME = 'bisonswap';
         $this->conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 
         // return database handler
