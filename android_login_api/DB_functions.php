ee9a9927b54da9ed6802fc55729c40d6fba35341
Patrick Fischer <fischerpl18@gmail.com>
2017-01-30 14:57:25 -0600

    Debugging PHP garbage

--- android_login_api/DB_functions.php
+++ android_login_api/DB_functions.php
@@ -62,16 +62,9 @@ class DB_Functions {
         $test = "SELECT * FROM users where email = '$email'";
         if($result = $this->conn->query($test)) {
           while($row = $result->fetch_row()) {
-            $encrypted_password = row[2];
+            $encrypted_password = $row[2];
             $userPass = hashSSHA($password);
-            echo $userPass."\n".$encrypted_password."\n";
-            if($userPass == $encrypted_password) {
-              $user = [];
-              for($i = 0; $i < count($result); $i++) {
-                $user[i] = $row[i];
-              }
-              echo var_dump($user);
-            }
+            echo $encrypted_password;
           }
           $result->close();
         }
