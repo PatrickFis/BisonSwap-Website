6575c36ad498b7aeeca97fe03e1b62f748ffa0f0
Patrick Fischer <fischerpl18@gmail.com>
2017-01-30 13:25:57 -0600

    Debug code

--- android_login_api/login.php
+++ android_login_api/login.php
@@ -14,20 +14,23 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
     // get the user by email and password
     $user = $db->getUserByEmailAndPassword($email, $password);
-    echo $user."\n";
+    echo "Got username and pass\n";
     if ($user != false) {
         // use is found
+        echo "User not found\n";
         $response["error"] = FALSE;
         $response["user"]["email"] = $user["email"];
         echo json_encode($response);
     } else {
         // user is not found with the credentials
+        echo "Credentials wrong\n";
         $response["error"] = TRUE;
         $response["error_msg"] = "Login credentials are wrong. Please try again!";
         echo json_encode($response);
     }
 } else {
     // required post params is missing
+    echo "Logged in\n";
     $response["error"] = TRUE;
     $response["error_msg"] = "Required parameters email or password is missing!";
     echo json_encode($response);
