ccf4eb0316ff01dabb149776ced3907d7c1957fa
Patrick Fischer <fischerpl18@gmail.com>
2017-02-06 14:15:07 -0600

    Firebase test commit

--- index.php
+++ index.php
@@ -9,17 +9,28 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body> 
+<body>
 
 <?php
     include 'navbar.php';
 ?>
-  
+
 <div class="container">
   <?php
     include 'sign-in-form.php';
   ?>
 </div>
-
+<script src="https://www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA0saZpdhgWuQ5MvD81I3K09M0Wbk31c6Q",
+    authDomain: "bisonswap-a0af2.firebaseapp.com",
+    databaseURL: "https://bisonswap-a0af2.firebaseio.com",
+    storageBucket: "bisonswap-a0af2.appspot.com",
+    messagingSenderId: "307753783953"
+  };
+  firebase.initializeApp(config);
+</script>
 </body>
 </html>
