b0f58d2d32d5f27e761b8a29aaace8873c383d60
Patrick Fischer <fischerpl18@gmail.com>
2017-02-08 10:34:37 -0600

    Commit of firebase work to use server's install of npm

--- index.php
+++ index.php
@@ -20,6 +20,10 @@
     include 'sign-in-form.php';
   ?>
 </div>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-messaging.js"></script>
 <script src="https://www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>
 <script>
   // Initialize Firebase
