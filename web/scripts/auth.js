b4af10394b8035cedbc30fabc1d6d9abf71ab754
Patrick Fischer <fischerpl18@gmail.com>
2017-02-15 12:44:07 -0600

    Debugging auth.js

--- web/scripts/auth.js
+++ web/scripts/auth.js
@@ -20,6 +20,8 @@ function FriendlyChat() {
   this.checkSetup();
 
   // Shortcuts to DOM Elements
+  this.userPic = document.getElementById('user-pic');
+  this.userName = document.getElementById('user-name');
   this.signInButton = document.getElementById('sign-in');
   this.signOutButton = document.getElementById('sign-out');
   this.signInSnackbar = document.getElementById('must-signin-snackbar');
@@ -61,7 +63,7 @@ FriendlyChat.prototype.onAuthStateChanged = function(user) {
     var userName = user.displayName;
 
     // Set the user's profile pic and name.
-    //this.userPic.style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
+    this.userPic.style.backgroundImage = 'url(' + (profilePicUrl || '/images/profile_placeholder.png') + ')';
     this.userName.textContent = userName;
 
     // Show user's profile and sign-out button.
