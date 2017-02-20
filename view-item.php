2cfadc658486b40c60182d7d9b79920d9394eaf0
Patrick Fischer <fischerpl18@gmail.com>
2017-02-20 16:56:23 -0600

    Testing database reads

--- view-item.php
+++ view-item.php
@@ -10,12 +10,12 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
-<body> 
+<body>
 
 <?php
     include 'navbar.php';
 ?>
-  
+
 <div class="container">
   	<div class="row">
 	  	<div class="col-sm-8">
@@ -58,7 +58,33 @@
 			</div>
 	  	</div>
   	</div>
+    <button id="test" onclick="readTest">Test!</button>
 </div>
 
 </body>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.2/firebase-messaging.js"></script>
+<script src="https://www.gstatic.com/firebasejs/3.6.8/firebase.js"></script>
+<script>
+  function readTest() {
+    var user = firebase.auth().currentUser.email;
+    return firebase.database().ref('items/' + user.email).once('value').then(function(snapshot) {
+      var itemName = snapshot.val().itemName;
+    });
+  }
+</script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA0saZpdhgWuQ5MvD81I3K09M0Wbk31c6Q",
+    authDomain: "bisonswap-a0af2.firebaseapp.com",
+    databaseURL: "https://bisonswap-a0af2.firebaseio.com",
+    storageBucket: "bisonswap-a0af2.appspot.com",
+    messagingSenderId: "307753783953"
+  };
+  firebase.initializeApp(config);
+</script>
+<script src="web/scripts/auth.js"></script>
 </html>
