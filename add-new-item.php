cebd6e642fc52ff4cc2334b560a65dfc02a6733f
Patrick Fischer <fischerpl@mail.lipscomb.edu>
2017-02-20 21:04:20 +0000

    Commit of other people's work

--- add-new-item.php
+++ add-new-item.php
@@ -40,6 +40,20 @@
               </select>
             </div>
             <div class="form-group">
+              <label for="condition">Item Condition:</label>
+              <select class="form-control" id="condition">
+                <option value disabled selected style="display: none;">-Select-</option>
+                <option>1 - Like new</option>
+                <option>2 - Very Good</option>
+                <option>3 - Good</option>
+                <option>4 - Fair</option>
+                <option>5 - Acceptable</option>
+                <option>6 - Has defects</option>
+                <option>7 - Slightly damaged</option>
+                <option>8 - Need repair</option>
+              </select>
+            </div>
+            <div class="form-group">
               <label for="item-description">Item Description:</label>
               <textarea class="form-control" rows="5" id="item-description"></textarea>
             </div>
@@ -64,16 +78,16 @@
             <div class="form-group">
               <hr />
             </div>
-            <button type="submit" class="btn btn-success">Submit</button>
+            <button type="submit" class="btn btn-success" onclick="addItem()">Submit</button>
           </form>
       </div>
     </div>
   </div>
 </body>
 <script>
-function addItem(userid, itemName, itemCategory, itemDescription, date, itemPictures, rating) {
+function addItem() {
   var pushData = {
-    userid: userid,
+    userid: ,
     itemName: itemName,
     itemCategory: itemCategory,
     itemDescription: itemDescription,
