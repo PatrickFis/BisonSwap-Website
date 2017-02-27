c545832e59317600aefbe550ebce1f9323ae9720
Patrick Fischer <fischerpl18@gmail.com>
2017-02-27 14:21:42 -0600

    Changing navbar link

--- navbar.php
+++ navbar.php
@@ -14,7 +14,7 @@
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 
       <ul class="nav navbar-nav">
-        <li><a href="recent-uploads.php">Link 1<span class="sr-only">(current)</span></a></li>
+        <li><a href="/web/chat.html">Chat<span class="sr-only">(current)</span></a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Link 2<span class="caret"></span></a>
           <ul class="dropdown-menu">
