8e16722246ce52440f3274a42720c8f89d55a765
Patrick Fischer <fischerpl18@gmail.com>
2017-03-11 09:39:03 -0600

    Debugging storage

--- add-new-item.php
+++ add-new-item.php
@@ -108,7 +108,7 @@ function addItem() {
   updates['/items/' + newPushKey] = pushData;
 
   var file = document.getElementById("pic-1").files[0];
-  var fileName = file.value;
+  var fileName = document.getElementById("pic-1").value;
   console.log('File name: ' + fileName);
   firebase.storage().ref().child('images/').put(file).then(function(snapshot) {
     console.log('Uploaded a blob or file!');
