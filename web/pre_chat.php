faa09fa75bb514408eff4f172a791ef61315b981
Patrick Fischer <fischerpl18@gmail.com>
2017-03-27 14:55:08 -0500

    Working on chat

--- web/pre_chat.php
+++ web/pre_chat.php
@@ -122,9 +122,11 @@
     // var innerHTMLArray = [];
     if(array.length > 0) {
       var string = '<a href="javascript:postwith("chat.php", {email:'+ firebase.auth().currentUser.email + ', email2:'+ array[0] + '})">' + array[0] + "</a><br>";
+      console.log(string);
       // var string = array[0] + '<button type="submit" class="btn btn-success" onclick="chat()">Submit</button><br>';
       for(var i = 1; i < array.length; i++) {
         string += '<a href="javascript:postwith("chat.php", {email:'+ firebase.auth().currentUser.email + ', email2:'+ array[i] + '})">' + array[i] + "</a><br>";
+        console.log(string);
         // innerHTMLArray[i] = string;
       }
   }
