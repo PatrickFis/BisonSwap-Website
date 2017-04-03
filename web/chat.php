0cb860e2f6e0337cfbd8ae17c4b1297b7f7c41ed
Patrick Fischer <fischerpl18@gmail.com>
2017-04-03 15:02:09 -0500

    Debugging chat

--- web/chat.php
+++ web/chat.php
@@ -52,8 +52,8 @@
   <link rel="stylesheet" href="styles/main.css">
 </head>
 <body>
-<input type=hidden id ="email1" name=email1 value="<?php echo $email1 ?>">
-<input type=hidden id ="email2" name=email2 value="<?php echo $email2 ?>">
+<input type=hidden id ="email1" name=email1 value="<?php echo $_POST($email1) ?>">
+<input type=hidden id ="email2" name=email2 value="<?php echo $_POST($email2) ?>">
 <div class="demo-layout mdl-layout mdl-js-layout mdl-layout--fixed-header">
 
   <!-- Header section containing logo -->
