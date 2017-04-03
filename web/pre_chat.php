e18b14a36b1d09ab38d49f72e0e3741702d7747d
Patrick Fischer <fischerpl18@gmail.com>
2017-04-03 15:15:25 -0500

    Debugging chat

--- web/pre_chat.php
+++ web/pre_chat.php
@@ -122,12 +122,13 @@
     // var innerHTMLArray = [];
     if(array.length > 0) {
 
-      // var string = "<a href=\"javascript:postwith(\"chat.html\", {email:\"+ firebase.auth().currentUser.email + \", email2:\"+ array[1] + \"})\">\" + array[1] + \"</a><br>";
-      var string = '<a href="javascript:postwith(\'chat.php\',{email1:\''+firebase.auth().currentUser.email+'\',email2:\''+array[1]+'\'})">'+array[1]+'</a><br>';
+      // var string = '<a href="javascript:postwith(\'chat.php\',{email1:\''+firebase.auth().currentUser.email+'\',email2:\''+array[1]+'\'})">'+array[1]+'</a><br>';
+      var string = '<a href="chat.php?email1=' + firebase.auth().currentUser.email + '&email2=' + array[1] + '>' + array[1] + '</a><br>';
       console.log(string);
       // var string = array[0] + '<button type="submit" class="btn btn-success" onclick="chat()">Submit</button><br>';
       for(var i = 2; i < array.length; i++) {
-        string += '<a href="javascript:postwith(\'chat.php\',{email1:\''+firebase.auth().currentUser.email+'\',email2:\''+array[i]+'\'})">'+array[i]+'</a><br>';
+        // string += '<a href="javascript:postwith(\'chat.php\',{email1:\''+firebase.auth().currentUser.email+'\',email2:\''+array[i]+'\'})">'+array[i]+'</a><br>';
+        string += '<a href="chat.php?email1=' + firebase.auth().currentUser.email + '&email2=' + array[i] + '>' + array[i] + '</a><br>';
         console.log(string);
         // innerHTMLArray[i] = string;
       }
